919d35753da9ec0e9c70be22a15266ff4c062c49
chamindujs <chamindujs@gmail.com>
2021-08-30 20:28:11 +0530

    Change styles

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Header from './Header'
 import Categories from './Categories'
 import Quotes from './Quotes'
+import Details from './Details';
+import Features from './Features';
+
 
 function Home() {
     const useStyles = makeStyles({
@@ -17,10 +20,14 @@ function Home() {
     return (
       <div className={classes.homeContainer}>
         <Header />
-        <div id="catt" className={classes.cats}><Categories  /></div>
+        <Features />
+        <Details />
+        <div id="catt" className={classes.cats}>
+          <Categories />
+        </div>
         <Quotes />
       </div>
-    )
+    );
 }
 
 
