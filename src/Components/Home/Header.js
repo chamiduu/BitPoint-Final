045cbc82a9364642cc396a04385c52b17c1197a7
chamindujs <chamindujs@gmail.com>
2021-08-30 14:13:42 +0530

    New svg

--- src/Components/Home/Header.js
+++ src/Components/Home/Header.js
@@ -7,38 +7,58 @@ import {ReactComponent as Logo} from '../../Img/hero.svg';
 
 export default function Header() {
     const useStyles = makeStyles({
-        headerContainer: {
-            width: '95%',
-            margin: 'auto',
-            marginTop: '8%',
-            [theme.breakpoints.down('md')]: {
-                marginTop: '4%'
-            },
-            [theme.breakpoints.down('xs')]: {
-                marginTop: '0'
-            },
-            
+      headerContainer: {
+        width: "95%",
+        height: "100vh",
+        margin: "auto",
+        display: "flex",
+        alignItems: "center",
+        [theme.breakpoints.down("md")]: {
+          marginTop: "4%",
         },
+        [theme.breakpoints.down("xs")]: {
+          marginTop: "0",
+        },
+      },
 
-        hero: {
-            padding: '40px',
-            paddingTop: '170px'
+      hero: {
+        padding: "40px",
+        paddingTop: "170px",
+      },
+      svg: {
+        display: "flex",
+        marginTop: "100px",
+        flexDirection: "column",
+        alignItems: "flex-end",
+        [theme.breakpoints.down("lg")]: {
+          alignItems: "center",
+          marginTop: "0px",
         },
-        svg: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-end',
-            [theme.breakpoints.down('lg')]: {
-                alignItems: 'center',
-            },
+      },
+      logo: {
+        [theme.breakpoints.down("lg")]: {
+          width: "100%",
+          height: "auto",
         },
-        logo: {
-            [theme.breakpoints.down('lg')]: {
-                width: '100%',
-            height: 'auto'
-            },
-        },
+      },
 
+      buttonI: {
+        color: "#fff",
+        border: "1px solid #fff",
+        "&:hover": {
+          color: "#fff",
+          border: "1px solid #fff",
+        },
+      },
+
+      buttonII: {
+        backgroundColor: "#eaeeff",
+        color: "#0363ff",
+        "&:hover": {
+          backgroundColor: "#eaeeff",
+          color: "#0363ff",
+        },
+      },
     });
     const classes = useStyles();
 
@@ -49,29 +69,71 @@ export default function Header() {
   
 
     return (
-        <div>
-            <Grid container className={classes.headerContainer}>
-                <Grid item  xl={6} lg={6} md={12} sm={12} xs={12} className={classes.hero}>
-                    <Typography variant='h2' gutterBottom="true" align='left' style={{marginBottom: '50px', fontWeight: 'bold'}}>BITS 2021</Typography>
+      <div>
+        <Grid container className={classes.headerContainer}>
+          <Grid
+            item
+            xl={6}
+            lg={6}
+            md={12}
+            sm={12}
+            xs={12}
+            className={classes.hero}
+          >
+            <Typography
+              variant="h2"
+              gutterBottom="true"
+              align="left"
+              style={{ marginBottom: "50px", fontWeight: "bold" }}
+            >
+              BITS 2021
+            </Typography>
 
-                    <div className="countdown">ADVANCED TASKS ARE NOW AVAILABLE !
-                    <br></br>
-                    It's your time to shine
-                    </div>
-
-                    <Typography variant='h5'  align='left' style={{marginBottom: '50px'}}><span style={{color: '#3ed1b8'}}>The First-Ever Task-Based Intra-School ICT competition,</span>  organized by the Ananda College ICT Society to provide a platform for our school's students to polish and improve their ICT knowledge in many fields of ICT</Typography>
-
-                    <Button variant='outlined' target="_blank" color='secondary' href='https://docs.google.com/forms/d/e/1FAIpQLSfJIXV6-3WYIpqv92mHcVcDfZpBnWPEizbTOJ18g61oOSL8gA/viewform'>Register Now</Button>
-                    <Button variant='contained' href='#catt' color='secondary' style={{color: '#000', marginLeft: '20px'}}>More Info</Button>
-                </Grid>
-                <Grid item xl={6} lg={6} md={12} sm={12} xs={12} className={classes.svg}>
-
-                <div className={classes.svgContainer}><Logo className={classes.logo}/></div> 
-
-                    
-                    
-                </Grid>
-            </Grid>
-        </div>
-    )
+            <Typography
+              variant="h5"
+              align="left"
+              style={{ marginBottom: "50px", color: "#91b8ff" }}
+            >
+              <span style={{ color: "#fff" }}>
+                The First-Ever Task-Based Intra-School ICT competition,
+              </span>{" "}
+              organized by the Ananda College ICT Society to provide a platform
+              for our school's students to polish and improve their ICT
+              knowledge in many fields of ICT
+            </Typography>
+            <Button
+              className={classes.buttonI}
+              variant="outlined"
+              target="_blank"
+              color="secondary"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfJIXV6-3WYIpqv92mHcVcDfZpBnWPEizbTOJ18g61oOSL8gA/viewform"
+            >
+              Register Now
+            </Button>
+            <Button
+              className={classes.buttonII}
+              variant="contained"
+              href="#catt"
+              color="secondary"
+              style={{ marginLeft: "20px" }}
+            >
+              More Info
+            </Button>
+          </Grid>
+          <Grid
+            item
+            xl={6}
+            lg={6}
+            md={12}
+            sm={12}
+            xs={12}
+            className={classes.svg}
+          >
+            <div className={classes.svgContainer}>
+              <Logo className={classes.logo} />
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+    );
 }
