b5a49a50e3696ffbfac4e681e18f087a8b84cebf
chamindujs <chamindujs@gmail.com>
2021-08-30 14:13:59 +0530

    Update quote authors

--- src/Components/Home/Quotes.js
+++ src/Components/Home/Quotes.js
@@ -12,28 +12,71 @@ function Quotes() {
 
     const classes = useStyles();
     return (
-        <div>
-            <div className={classes.quoteContainer}>
-                <Typography align='left' variant='h4'> <span style={{color: '#3ed1b8', fontSize: '50px'}}>“</span> If it wasn't for BITS, I could've missed so many wonderful opportunities <span style={{color: '#3ed1b8', fontSize: '50px'}}>”</span> </Typography>
-                <Typography align='left' variant='h6' style={{marginLeft: '30px'}}>~ Chamindu Jayanath</Typography>
-                <Typography align='left' variant='h6' style={{marginLeft: '45px', color: '#ffffffb3'}}>BITS '19 Winner</Typography>
-            </div>
-
-            <div className={classes.quoteContainer}>
-                <Typography align='right' variant='h4'> <span style={{color: '#3ed1b8', fontSize: '50px'}}>“</span> I think BITS will have a great impact on the students who like the technical side of our lives <span style={{color: '#3ed1b8', fontSize: '50px'}}>”</span> </Typography>
-                <Typography align='right' variant='h6' style={{marginRight: '30px'}}>~ Chamuthu Edirisuriya</Typography>
-                <Typography align='right' variant='h6' style={{marginRight: '45px', color: '#ffffffb3'}}>President of ACICTS</Typography>
-            </div>
-
-            <div className={classes.quoteContainer}>
-                <Typography align='left' variant='h4'> <span style={{color: '#3ed1b8', fontSize: '50px'}}>“</span>Enthusiasm and commitment made me a winner. That victory was my turning point. I also invite you to be a winner too. <span style={{color: '#3ed1b8', fontSize: '50px'}}>”</span> </Typography>
-                <Typography align='left' variant='h6' style={{marginRight: '30px'}}>~ Gothira Mendis
-</Typography>
-                <Typography align='left' variant='h6' style={{marginRight: '45px', color: '#ffffffb3'}}>BITS '18 Winner</Typography>
-            </div>
-
+      <div>
+        <div className={classes.quoteContainer}>
+          <Typography align="left" variant="h4">
+            {" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>“</span> If it
+            wasn't for BITS, I could've missed so many wonderful opportunities{" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>”</span>{" "}
+          </Typography>
+          <Typography align="left" variant="h6" style={{ marginLeft: "30px" }}>
+            ~ Chamindu Jayanath
+          </Typography>
+          <Typography
+            align="left"
+            variant="h6"
+            style={{ marginLeft: "45px", color: "#ffffffb3" }}
+          >
+            Developer of BitPoint
+          </Typography>
         </div>
-    )
+
+        <div className={classes.quoteContainer}>
+          <Typography align="right" variant="h4">
+            {" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>“</span> I
+            think BITS will have a great impact on the students who like the
+            technical side of our lives{" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>”</span>{" "}
+          </Typography>
+          <Typography
+            align="right"
+            variant="h6"
+            style={{ marginRight: "30px" }}
+          >
+            ~ Gothira Mendis
+          </Typography>
+          <Typography
+            align="right"
+            variant="h6"
+            style={{ marginRight: "45px", color: "#ffffffb3" }}
+          >
+            President of ACICTS
+          </Typography>
+        </div>
+
+        <div className={classes.quoteContainer}>
+          <Typography align="left" variant="h4">
+            {" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>“</span>
+            Enthusiasm and commitment made me a winner. That victory was my
+            turning point. I also invite you to be a winner too.{" "}
+            <span style={{ color: "#3ed1b8", fontSize: "50px" }}>”</span>{" "}
+          </Typography>
+          <Typography align="left" variant="h6" style={{ marginRight: "30px" }}>
+            ~ Manula Thejan
+          </Typography>
+          <Typography
+            align="left"
+            variant="h6"
+            style={{ marginRight: "45px", color: "#ffffffb3" }}
+          >
+            Manula Thejan
+          </Typography>
+        </div>
+      </div>
+    );
 }
 
 export default Quotes
