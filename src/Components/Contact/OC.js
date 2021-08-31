0b71fa2cd014bc2c65a9b33f54fbea3494523a35
chamindujs <chamindujs@gmail.com>
2021-08-31 23:16:22 +0530

    Update OC

--- src/Components/Contact/OC.js
+++ src/Components/Contact/OC.js
@@ -4,11 +4,9 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography'
 import { Grid } from '@material-ui/core';
 import { GitHub, Instagram, Phone } from '@material-ui/icons';
 import theme from '../../theme'
-import OCdata from '../../Data/OC.json'
 import Aos from 'aos';
 import "aos/dist/aos.css"
 
